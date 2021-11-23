bdf1b24383df4d4fa545ff4b24a3b80214faf20c
Shengwen Cheng <c.shengwen.tw@gmail.com>
2021-11-24 01:15:10 +0800

    complete state propagation of the simulator

--- bldc_dynamics.m
+++ bldc_dynamics.m
@@ -1,38 +1,52 @@
 classdef bldc_dynamics
     properties
+        dt;               %time period of one iteration
+        
         R = 0.7;          %resistance [ohm]
         L = 2.72;         %self-inductance [mH]
         M = 1.5;          %mutual-inductance [mH]
         lambda_m = 0.105; %flux linkage of the motor [wb]
         P = 4;            %number of the motor poles
-        Jm = 0.000284;    %inertia of the motor [kg*m/sec^2]
-        Jl = 0;           %inertia of the load [kg*m/sec^2]
-        J;                %J = Jm + Jl
-        B_vis = 0.02;     %motor viscous friction constant [N*m/rad/sec]
+        Jm = 0.000284;    %inertia of the motor [kg*m/s^2]
+        Jl = 0;           %inertia of the load [kg*m/s^2]
+        J = 0;            %J = Jm + Jl
+        B_vis = 0.02;     %motor viscous friction constant [N*m/rad/s]
         Kt = 1;           %torque-current constant
         
-        fa;               %back EMF of the phase A
-        fb;               %back EMF of the phase B
-        fc;               %back EMF of the phase C
+        f_a = 0;          %back EMF of the phase A
+        f_b = 0;          %back EMF of the phase B
+        f_c = 0;          %back EMF of the phase C
         
         %matrices of the LTV (Linear Time Variant) system
         A = zeros(5, 5);  %state transition matrix
         B = zeros(5, 4);  %control matrix
         C = zeros(5, 3);  %back emf matrix
         
-        x = zeros(5, 1);  %state vector = [i_a; i_b; i_c; omega_m; theta_r]
+        %state vector = [i_a; i_b; i_c; omega_m; theta_r]
+        x = [0;            %initial phase A current
+             0;            %initial phase B current
+             0;            %initial phase C current
+             deg2rad(10);  %initial motor speed
+             deg2rad(50)]; %initial motor position
         u = zeros(4, 1);  %control vector = [v_a; v_b; v_c; T_l]
         e = zeros(3, 1);  %back emf vector = [e_a; e_b; e_c]        
     end
     
     methods
-        function ret_obj = init(obj)            
+        function ret_obj = init(obj, dt)
+            obj.dt = dt;
+            
             obj.J = obj.Jm + obj.Jl;
             
             obj.A(1, 1) = -obj.R/(obj.L-obj.M);
             obj.A(2, 2) = -obj.R/(obj.L-obj.M);
             obj.A(3, 3) = -obj.R/(obj.L-obj.M);
+            %
+            %A(4, 1) = lambda_div_J * obj.f_a; %change dynamically
+            %A(4, 2) = lambda_div_J * obj.f_b; %change dynamically
+            %A(4, 3) = lambda_div_J * obj.f_c; %change dynamically
             obj.A(4, 4) = -obj.B_vis/obj.J;
+            %
             obj.A(5, 4) = obj.P/2;
             
             obj.B(1, 1) = 1/(obj.L-obj.M);
@@ -47,26 +61,52 @@ classdef bldc_dynamics
             ret_obj = obj;
         end
         
-        function x_dot = update_dynamics(obj)
-            %update the back EMF according to the motor angle
-            obj.fa = obj.back_emf_fa(obj.x(5));
-            obj.fb = obj.back_emf_fb(obj.x(5));
-            obj.fc = obj.back_emf_fc(obj.x(5));
-            
+        function x_dot = update_dynamics(obj)                        
             %A matrix is time variant, update the entry corresponding to
             %the back EMF
-            lambda_div_J = obj.lambda_m/obj.J;
-            A(4, 1) = lambda_div_J * obj.fa;
-            A(4, 2) = lambda_div_J * obj.fb;
-            A(4, 3) = lambda_div_J * obj.fc;
+            lambda_div_J = obj.lambda_m / obj.J;
+            A(4, 1) = lambda_div_J * obj.f_a;
+            A(4, 2) = lambda_div_J * obj.f_b;
+            A(4, 3) = lambda_div_J * obj.f_c;
             
             %x_dot = Ax + Bu + Ce
             x_dot = obj.A*obj.x + obj.B*obj.u + obj.C*obj.e;
         end
         
-        function retval = back_emf_fa(obj, theta_r)
-            theta_r = mod(theta_r, 2*pi); %restrict theta_r in [0, 2*pi]
+        function f_next = integrator(obj, f_now, f_dot, dt)
+            %eulers method
+            f_next = [f_now(1) + (f_dot(1) * dt);
+                      f_now(2) + (f_dot(2) * dt);
+                      f_now(3) + (f_dot(3) * dt);
+                      f_now(4) + (f_dot(4) * dt);
+                      f_now(5) + (f_dot(5) * dt)];
+        end
+        
+        function ret_obj = update(obj)
+            %update the back EMF according to the motor angle
+            obj.f_a = obj.back_emf_fa(obj.x(5));
+            obj.f_b = obj.back_emf_fb(obj.x(5));
+            obj.f_c = obj.back_emf_fc(obj.x(5));
+            %            
+            omega_times_lambda = obj.x(4) * obj.lambda_m;
+            obj.e(1) = omega_times_lambda * obj.f_a;
+            obj.e(2) = omega_times_lambda * obj.f_b;
+            obj.e(3) = omega_times_lambda * obj.f_c;
             
+            x_dot = obj.update_dynamics();
+            obj.x = obj.integrator(obj.x, x_dot, obj.dt);
+            
+            %restrict motor position in +-pi
+            obj.x(5) = mod(obj.x(5), 2*pi);
+            
+            ret_obj = obj;
+        end
+        
+        function retval = back_emf_fa(obj, theta_r)
+            %restrict theta_r in [0, 2*pi]
+            theta_r = mod(theta_r, 2*pi);
+            
+            %generate trapezoidal signal
             if (theta_r >= 0) && (theta_r < pi/6)
                 retval = theta_r * 6 / pi;
             elseif (theta_r >= pi/6) && (theta_r < 5*pi/6)
@@ -81,12 +121,22 @@ classdef bldc_dynamics
         end
         
         function retval = back_emf_fb(obj, theta_r)
+            %phase shifting
             theta_r = theta_r - deg2rad(120);
+            
+            %restrict theta_r in [0, 2*pi]
+            theta_r = mod(theta_r, 2*pi);
+            
             retval = back_emf_fa(obj, theta_r);
         end
         
         function retval = back_emf_fc(obj, theta_r)
+            %phase shifting
             theta_r = theta_r - deg2rad(240);
+            
+            %restrict theta_r in [0, 2*pi]
+            theta_r = mod(theta_r, 2*pi);
+            
             retval = back_emf_fa(obj, theta_r);
         end
     end
--- main.m
+++ main.m
@@ -1,40 +1,131 @@
 close all;
 
+dt = 0.001;
 ITERATION_TIMES = 10000;
 
 bldc = bldc_dynamics;
-bldc = bldc.init();
+bldc = bldc.init(dt);
 
+%3-phase currents
+i_a = zeros(1, ITERATION_TIMES);
+i_b = zeros(1, ITERATION_TIMES);
+i_c = zeros(1, ITERATION_TIMES);
+%motor speed
+omega_m = zeros(1, ITERATION_TIMES);
+%motor position
 theta_r = zeros(1, ITERATION_TIMES);
-fa = zeros(1, ITERATION_TIMES);
-fb = zeros(1, ITERATION_TIMES);
-fc = zeros(1, ITERATION_TIMES);
+%3-phase back EMF
+e_a = zeros(1, ITERATION_TIMES);
+e_b = zeros(1, ITERATION_TIMES);
+e_c = zeros(1, ITERATION_TIMES);
+%normalized 3-phase back EMF
+e_a = zeros(1, ITERATION_TIMES);
+e_b = zeros(1, ITERATION_TIMES);
+e_c = zeros(1, ITERATION_TIMES);
+
+%time sequence
+time_arr = zeros(1, ITERATION_TIMES);
 
 for i = 1: ITERATION_TIMES
-    theta_r(i) = (i - 1) * ((2 * pi) / ITERATION_TIMES);
+    bldc = bldc.update();
+
+    %currents of motor phases
+    i_a(i) = bldc.x(1);
+    i_b(i) = bldc.x(2);
+    i_c(i) = bldc.x(3);
     
-    fa(i) = bldc.back_emf_fa(theta_r(i));
-    fb(i) = bldc.back_emf_fb(theta_r(i));
-    fc(i) = bldc.back_emf_fc(theta_r(i));
+    %motor speed
+    omega_m(i) = bldc.x(4);
+    %motor position
+    theta_r(i) = bldc.x(5);
+    
+    %back EMF
+    e_a(i) = bldc.e(1);
+    e_b(i) = bldc.e(2);
+    e_c(i) = bldc.e(3);
+    
+    %normalized back EMF
+    f_a(i) = bldc.f_a;
+    f_b(i) = bldc.f_b;
+    f_c(i) = bldc.f_c;
+    
+    %time sequence
+    time_arr(i) = (i - 1) * dt;
 end
 
-%position
+%3-phase back EMF
+figure('Name', 'Current');
+subplot (3, 1, 1);
+plot(time_arr(:), i_a(:));
+xlim([0 time_arr(end)]);
+ylim([-1.3 1.3]);
+xlabel('time [s]');
+ylabel('i_a');
+subplot (3, 1, 2);
+plot(time_arr(:), i_b(:));
+xlim([0 time_arr(end)]);
+ylim([-1.3 1.3]);
+xlabel('time [s]');
+ylabel('i_b');
+subplot (3, 1, 3);
+plot(time_arr(:), i_c(:));
+xlim([0 time_arr(end)]);
+ylim([-1.3 1.3]);
+xlabel('time [s]');
+ylabel('i_c');
+
+figure('Name', 'omega_m');
+plot(time_arr(:), omega_m(:));
+xlim([0 time_arr(end)]);
+ylim([-20 20]);
+xlabel('time [s]');
+ylabel('motor speed');
+
+figure('Name', 'theta_r');
+plot(time_arr(:), rad2deg(theta_r(:)));
+xlim([0 time_arr(end)]);
+ylim([-200 200]);
+xlabel('time [s]');
+ylabel('motor position');
+
+%3-phase back EMF
 figure('Name', 'Back EMF');
 subplot (3, 1, 1);
-plot(rad2deg(theta_r(:)), fa(:));
-xlim([0 rad2deg(theta_r(end))]);
-ylim([-1.3 1.3]);
-xlabel('theta_r');
-ylabel('f_a');
+plot(time_arr(:), e_a(:));
+xlim([0 time_arr(end)]);
+ylim([-5 15]);
+xlabel('time [s]');
+ylabel('e_a');
 subplot (3, 1, 2);
-plot(rad2deg(theta_r(:)), fb(:));
-xlim([0 rad2deg(theta_r(end))]);
-ylim([-1.3 1.3]);
-xlabel('theta_r');
-ylabel('f_b');
+plot(time_arr(:), e_b(:));
+xlim([0 time_arr(end)]);
+ylim([-15 15]);
+xlabel('time [s]');
+ylabel('e_b');
 subplot (3, 1, 3);
-plot(rad2deg(theta_r(:)), fc(:));
-xlim([0 rad2deg(theta_r(end))]);
+plot(time_arr(:), e_c(:));
+xlim([0 time_arr(end)]);
+ylim([-15 15]);
+xlabel('time [s]');
+ylabel('e_c');
+
+%3-phase normalized back EMF
+figure('Name', 'Normalized back EMF');
+subplot (3, 1, 1);
+plot(time_arr(:), f_a(:));
+xlim([0 time_arr(end)]);
 ylim([-1.3 1.3]);
-xlabel('theta_r');
-ylabel('f_c');
+xlabel('time [s]');
+ylabel('e_a');
+subplot (3, 1, 2);
+plot(time_arr(:), f_b(:));
+xlim([0 time_arr(end)]);
+ylim([-1.3 1.3]);
+xlabel('time [s]');
+ylabel('e_b');
+subplot (3, 1, 3);
+plot(time_arr(:), f_c(:));
+xlim([0 time_arr(end)]);
+ylim([-1.3 1.3]);
+xlabel('time [s]');
+ylabel('e_c');
